182293280552493ff83f0fed23e7a137d029b3b2
Leo <leojunebedeo@gmail.com>
2023-03-10 18:41:52 +0800

    Get date/time back from RTC

--- fair-fare.ino
+++ fair-fare.ino
@@ -1,4 +1,3 @@
-#include <TimeLib.h>
 #include <LiquidCrystal_I2C.h>
 #include <virtuabotixRTC.h>
 #include <SoftwareSerial.h>
@@ -20,6 +19,7 @@ const int RTC_DAT_PIN = 6;
 const int RTC_RST_PIN = 7;
 
 String phone_number = "+639311235160";
+String current_phone_number = "+639617789314";
 
 TinyGPSPlus gps;
 SoftwareSerial gpsSerial(GPS_RX_PIN, GPS_TX_PIN);
@@ -45,22 +45,21 @@ const float succeeding_meter_rate = 0.005;
 const float wheel_circumference = 1.90;   // Meters
 
 float distance_travelled = 0;
-float total_FARE = 10;
+float total_fare = 10;
 float succeeding_distance = 0;
 
-char Time[]  = "Time: 00:00:00";
-char Date[]  = "Date: 00-00-00";
-byte last_second, Second, Minute, Hour, Day, Month;
-int Year;
-
-#define time_offset 28800  // define a clock offset of 3600 seconds (1 hour) ==> UTC + 1
+char incoming_char = 0;
+String sms_content = "";
 
 void setup() {
-  
-  Serial.begin(19200);
+  // Set RTC current date, and time in the following format:
+  // seconds, minutes, hours, day of the week, day of the month, month, year
+  //  myRTC.setDS1302Time(0, 36, 15, 4, 8, 3, 2023);
+
   pinMode(BUTTON_PIN,INPUT_PULLUP);
   pinMode(HALL_SENSOR_PIN,INPUT);
-
+  
+  Serial.begin(19200);
   lcd.init();
   lcd.backlight();
   lcd_print(5, 0, "FAIR  FARE");
@@ -68,22 +67,83 @@ void setup() {
   Serial.println("FAIR FARE");
   Serial.println("Getting ready");
 
+  SIM900.begin(115200);
+  delay(500);
+
+  // AT command to set SIM900 to SMS mode
+  SIM900.print("AT+CMGF=1\r"); 
+  delay(100);
+  
+  // Set module to send SMS data to serial out upon receipt 
+  SIM900.print("AT+CNMI=2,2,0,0,0\r");
+  delay(400);
+
+  if(SIM900.available() > 0) {
+    if (SIM900.read() > 0) {
+      while (SIM900.available() > 0) {
+        char c = SIM900.read();
+      }
+    }
+  }
+
   gpsSerial.begin(9600);
   delay(1000);
-
-  SIM900.begin(115200);
-  delay(1000);
   
-//  lcd_print(5, 0, "FAIR  FARE");
   lcd_print(3, 1, "");
   lcd_print(3, 2, "");
   Serial.println("FAIR FARE Ready!");
 }
 
 void loop() {
-//  printDateTime();
+  printDateTime();
+  read_sms();
   odometer_loop();
-  read_gps();
+//  read_gps();
+}
+
+void read_sms() {
+  SIM900.listen();
+  
+  if(SIM900.available() > 0) {  
+    //    while (SIM900.available() > 0) {
+    //      incoming_char = SIM900.read();
+    //      sms_content += String(incoming_char);  
+    //      delay(10);    
+    //    }
+    //
+    //    Serial.println(sms_content);
+    //    
+    //    if (sms_content.indexOf("Location") > 0 || sms_content.indexOf("location") > 0) {
+    //      Serial.println("Location request received");
+    //      Serial.println("Reading GPS...");
+    //      lcd.setCursor(0, 1);
+    //      lcd.print("  Location request  ");
+    //      lcd.setCursor(0, 2);
+    //      lcd.print("      received      ");
+    //      lcd.setCursor(0, 3);
+    //      lcd.print("   Reading GPS...   ");
+    //      read_gps();
+    //      sendSMS();
+    //    }
+    //
+    //    incoming_char = 0;
+    //    sms_content = "";
+    if (SIM900.read() > 0) {
+      while (SIM900.available() > 0) {
+        char c = SIM900.read();
+      }
+      Serial.println("Location request received");
+      Serial.println("Reading GPS...");
+      lcd.setCursor(0, 1);
+      lcd.print("  Location request  ");
+      lcd.setCursor(0, 2);
+      lcd.print("      received      ");
+      lcd.setCursor(0, 3);
+      lcd.print("   Reading GPS...   ");
+      read_gps();
+      sendSMS();
+    }
+  }
 }
 
 void odometer_loop() {
@@ -98,7 +158,7 @@ void odometer_loop() {
         if (millis() - millis_start > button_release_interval) {
           if (digitalRead(BUTTON_PIN) == HIGH) {
             delay(2000);
-            start_FARE_solving();
+            start_fare_solving();
             break;
           }
         }
@@ -107,16 +167,16 @@ void odometer_loop() {
   }
 }
 
-void start_FARE_solving() {
+void start_fare_solving() {
   pulse_count = 0;
   distance_travelled = 0;
-  total_FARE = 10;
+  total_fare = 10;
   succeeding_distance = 0;
 
 //  lcd_print(5, 0, "FAIR  FARE");
   lcd_print(0, 1, "");
   lcd_print(0, 2, "Distance: " + String(distance_travelled) + "Km");
-  lcd_print(0, 3, "FARE: Php" + String(total_FARE));
+  lcd_print(0, 3, "Fare: Php" + String(total_fare));
   
   while(true)  {
     int sensor_value = digitalRead(HALL_SENSOR_PIN);
@@ -131,15 +191,15 @@ void start_FARE_solving() {
         sensor_value = digitalRead(HALL_SENSOR_PIN);
         if (sensor_value == LOW) {
           pulse_count++;
-          calculate_FARE(pulse_count);
+          calculate_fare(pulse_count);
           lcd.setCursor(10, 2);
           lcd.print(String(distance_travelled) + "Km");
           lcd.setCursor(6, 3);
-          lcd.print("Php" + String(total_FARE));
+          lcd.print("Php" + String(total_fare));
           Serial.print("Distance(Km): ");
           Serial.print(String(distance_travelled));
           Serial.print(" FARE(Php): ");
-          Serial.println(String(total_FARE));
+          Serial.println(String(total_fare));
           hall_sensor_last_value = true;
           break;
         }
@@ -160,16 +220,16 @@ void start_FARE_solving() {
         Serial.println("Thank you!");
         delay(2000);
         lcd_print(0, 2, "");
-        bool end_FARE_solving = false;
+        bool end_fare_solving = false;
         while(true) {
           if (millis() - millis_start > button_release_interval) {
             if (digitalRead(BUTTON_PIN) == HIGH) {
-              end_FARE_solving = true;
+              end_fare_solving = true;
               break;
             }
           }
         }
-        if (end_FARE_solving) {
+        if (end_fare_solving) {
           break;
         }
       }
@@ -177,13 +237,13 @@ void start_FARE_solving() {
   }
 }
 
-void calculate_FARE(long total_pulse) {
+void calculate_fare(long total_pulse) {
   distance_travelled = (wheel_circumference * total_pulse) / 1000;
   if ((distance_travelled * 1000) > min_distance_travelled) {
     succeeding_distance = (distance_travelled * 1000) - min_distance_travelled;
-    total_FARE = (succeeding_distance * 0.005) + 10; 
+    total_fare = (succeeding_distance * 0.005) + 10; 
   } else {
-    total_FARE = 10;
+    total_fare = 10;
   }
 }
 
@@ -194,131 +254,21 @@ void lcd_print(int vertical_location, int horizontal_location, String text) {
   lcd.print(text);
 }
 
-void printDateTime() {
-  
-  myRTC.updateTime();
-  
-  Serial.print("Current Date / Time: ");
-  Serial.print(myRTC.dayofmonth);
-  Serial.print("/");
-  Serial.print(myRTC.month);
-  Serial.print("/");
-  Serial.print(myRTC.year);
-  Serial.print(" ");
-  Serial.print(myRTC.hours);
-  Serial.print(":");
-  Serial.print(myRTC.minutes);
-  Serial.print(":");
-  Serial.println(myRTC.seconds);
-
-  lcd.setCursor(3, 2);
-  lcd.print("Date: " + 
-    (myRTC.month < 10 ? "0" + String(myRTC.month) : String(myRTC.month)) + "-" + 
-    (myRTC.dayofmonth < 10 ? "0" + String(myRTC.dayofmonth) : String(myRTC.dayofmonth)) + "-" + 
-    String(myRTC.year).substring(2));
-  lcd.setCursor(3, 3);
-  lcd.print("Time: " + 
-    (myRTC.hours < 10 ? "0" + String(myRTC.hours) : String(myRTC.hours)) + ":" + 
-    (myRTC.minutes < 10 ? "0" + String(myRTC.minutes) : String(myRTC.minutes)) + ":" + 
-    (myRTC.seconds < 10 ? "0" + String(myRTC.seconds) : String(myRTC.seconds)));
-}
-
-void print_date_time_from_gps() {
-
-  // get time from GPS module
-  if (gps.time.isValid())
-  {
-    Minute = gps.time.minute();
-    Second = gps.time.second();
-    Hour   = gps.time.hour();
-  }
-
-  // get date drom GPS module
-  if (gps.date.isValid())
-  {
-    Day   = gps.date.day();
-    Month = gps.date.month();
-    Year  = gps.date.year();
-  }
-
-  if(last_second != gps.time.second())  // if time has changed
-  {
-    last_second = gps.time.second();
-
-    // set current UTC time
-    setTime(Hour, Minute, Second, Day, Month, Year);
-    // add the offset to get local time
-    adjustTime(time_offset);
-
-    // update time array
-    Time[12] = second() / 10 + '0';
-    Time[13] = second() % 10 + '0';
-    Time[9]  = minute() / 10 + '0';
-    Time[10] = minute() % 10 + '0';
-    Time[6]  = hour()   / 10 + '0';
-    Time[7]  = hour()   % 10 + '0';
-
-    // update date array
-    Date[12] = (year()  / 10) % 10 + '0';
-    Date[13] =  year()  % 10 + '0';
-    Date[6]  =  month() / 10 + '0';
-    Date[7] =  month() % 10 + '0';
-    Date[9]  =  day()   / 10 + '0';
-    Date[10]  =  day()   % 10 + '0';
-
-    // print time & date
-    print_wday(weekday());   // print day of the week
-    lcd.setCursor(3, 2);     // move cursor to column 0 row 2
-    lcd.print(Date);         // print time (HH:MM:SS)
-    lcd.setCursor(3, 3);     // move cursor to column 0 row 3
-    lcd.print(Time);         // print date (DD-MM-YYYY)
-  }
-  
-  Serial.print(Date);
-  Serial.print(" - ");
-  Serial.println(Time);
-}
-
-// function for displaying day of the week
-void print_wday(byte wday)
-{
-  lcd.setCursor(5, 1);  // move cursor to column 5, row 1
-  switch(wday)
-  {
-    case 1:  lcd.print("  Sunday  ");   break;
-    case 2:  lcd.print("  Monday  ");   break;
-    case 3:  lcd.print(" Tuesday ");   break;
-    case 4:  lcd.print("Wednesday");   break;
-    case 5:  lcd.print("Thursday ");   break;
-    case 6:  lcd.print("  Friday  ");   break;
-    default: lcd.print("Saturday ");
-  }
-
-}
-
 void read_gps() {
 
   gpsSerial.listen();
-
   bool break_flag = false;
-
+  
   while (true) {
-
     while (gpsSerial.available()) {
-    
       if (gps.encode(gpsSerial.read())) {
-        
-        displayGpsInfo();
-        print_date_time_from_gps();
-        
+        displayGpsInfo();      
         break_flag = true;
-        
         break;
       }
     }
 
     if (break_flag) {
-
       break;
     }
   }
@@ -344,4 +294,70 @@ void displayGpsInfo() {
 //    lcd.setCursor(0, 3);
 //    lcd.print("Not Available");
   }
+}
+
+void printDateTime() {
+  
+  myRTC.updateTime();
+  
+  Serial.print("Current Date / Time: ");
+  Serial.print(myRTC.dayofmonth);
+  Serial.print("/");
+  Serial.print(myRTC.month);
+  Serial.print("/");
+  Serial.print(myRTC.year);
+  Serial.print(" ");
+  Serial.print(myRTC.hours);
+  Serial.print(":");
+  Serial.print(myRTC.minutes);
+  Serial.print(":");
+  Serial.println(myRTC.seconds);
+
+  lcd.setCursor(0, 1);
+  lcd.print("                    ");
+  lcd.setCursor(0, 2);
+  lcd.print("   Date: " + 
+    (myRTC.month < 10 ? "0" + String(myRTC.month) : String(myRTC.month)) + "-" + 
+    (myRTC.dayofmonth < 10 ? "0" + String(myRTC.dayofmonth) : String(myRTC.dayofmonth)) + "-" + 
+    String(myRTC.year).substring(2));
+  lcd.setCursor(0, 3);
+  lcd.print("   Time: " + 
+    (myRTC.hours < 10 ? "0" + String(myRTC.hours) : String(myRTC.hours)) + ":" + 
+    (myRTC.minutes < 10 ? "0" + String(myRTC.minutes) : String(myRTC.minutes)) + ":" + 
+    (myRTC.seconds < 10 ? "0" + String(myRTC.seconds) : String(myRTC.seconds)));
+}
+
+void sendSMS() {
+
+  Serial.println("Sending location...");
+  lcd.setCursor(0, 3);
+  lcd.print("Sending location... ");
+
+  // USE INTERNATIONAL FORMAT CODE FOR MOBILE NUMBERS
+  SIM900.println("AT + CMGS = \"" + phone_number + "\"");
+  delay(100);
+
+  if (gps.location.isValid()) {
+    SIM900.println("Location: http://maps.google.com/maps?q=" + String(gps.location.lat(), 6) + "," + String(gps.location.lng(), 6) + "&ll=" + String(gps.location.lat(), 6) + "," + String(gps.location.lng(), 6) + "&z=17");
+  } else {
+    SIM900.println("Location: Not available. Please try again later.");
+  }
+  
+  delay(100);
+
+  // End AT command with a ^Z, ASCII code 26
+  SIM900.println((char)26);
+  delay(100);
+  SIM900.println();
+
+  delay(3000);
+
+  Serial.println("Location Sent");
+  lcd.setCursor(0, 3);
+  lcd.print("   Location Sent    ");
+
+  // Set module to send SMS data to serial out upon receipt 
+  SIM900.print("AT+CNMI=2,2,0,0,0\r");
+  
+  delay(3000);
 }
